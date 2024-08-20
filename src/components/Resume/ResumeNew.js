e43e5796257eea0a3e6c9ad9bfed28c101951d96
sudipta57 <ghoramiswapna32@gmail.com>
2024-08-20 20:27:47 +0530

    adding resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/My_resume.pdf";
+import pdf from "../../Assets/new_resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
